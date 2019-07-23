39322ccb22504128beed811371d794d3f499b1f0
Andrew Kennedy <andrew.kennedy@saucelabs.com>
2019-07-23 19:01:53 +0000

    init commit

---
+++ src/test.js
@@ -0,0 +1,46 @@
+const { Builder, By, Key } = require('selenium-webdriver')
+const username = process.env.USERNAME
+const accessKey = process.env.ACCESS_KEY
+const baseUrl = 'http://saucelabs.github.io/training-test-page'
+
+async function example() {
+    const driver = await new Builder().withCapabilities({
+        browserName: 'safari',
+        version: '11.1',
+        platformName: 'macOS 10.13',
+        'sauce:options': {
+            username,
+            accessKey,
+            build: 'Onboarding Sample App - NodeJS',
+            name: '1-first-test'
+        }
+    }).usingServer('https://@ondemand.saucelabs.com:443/wd/hub').build()
+
+    const comment = 'whale hello there'
+
+    try {
+        await driver.get(baseUrl)
+        const comments = await driver.findElement(By.id('comments'))
+        await comments.sendKeys(comment)
+
+        const send = await driver.findElement(By.id('submit'))
+        await send.sendKeys(Key.RETURN)
+
+        const yourComments = await driver.findElement(By.id('your_comments'))
+        const commentText = await yourComments.getText()
+
+        if (commentText === `Your comments: ${comment}`) {
+            driver.executeScript('sauce:job-result=passed')
+            console.log("Test Passed!")
+        } else {
+            driver.executeScript('sauce:job-result=failed')
+            console.log('Test Failed! Comments do not match.')
+        }
+    } catch (e) {
+        console.log('Test Failed!', e)
+    } finally {
+        await driver.quit()
+    }
+}
+
+example()
